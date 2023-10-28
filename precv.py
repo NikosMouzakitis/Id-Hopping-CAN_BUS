ecdea2fbfa0696d5f6dfd162c11f460da6d5d845
NikosMouzakitis <mzktsn@gmail.com>
2023-10-28 13:50:04 +0300

    implementation of animation on the receiver's side of Python program. Visualization utilizing matplotlib
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- precv.py
+++ precv.py
@@ -1,6 +1,22 @@
 import can
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+import threading
 
-def receive_can_messages(channel='vcan0'):
+#update with new values
+def update_data(new_value):
+    global data
+    data = np.roll(data,-1) #shift data to the left.
+    data[-1] = new_value ##add the new random value to the end
+
+def update_plot(frame):
+  # 
+    line.set_data(np.arange(360), data)
+    return line,
+
+
+def receive_can_and_update_animation(channel='vcan0'):
     bus = can.interface.Bus(channel=channel, bustype='socketcan')
     
     try:
@@ -9,12 +25,36 @@ def receive_can_messages(channel='vcan0'):
             message = bus.recv()  #receive message
             print(f"Received Message on Channel {channel}:")
             print(message)
+            #new_value = int.from_bytes(message.data[-2:0],byteorder='little') 
+            new_value = message.data[7] + (2*2*2*2*message.data[6])
+            
+            print("new value is: ",new_value)
+            ##ANIMATION UPDATE
+            update_data(new_value)
+            ani.event_source.start() #starts the animation update.
+        
     except KeyboardInterrupt:
         print("Receiver terminated by user")
 
+
+
+
+
+
 print("Python receiver starts execution")
+data = np.zeros(360)
 
-receive_can_messages()
+# creation of the figure.
+fig, ax = plt.subplots()
+ax.set_xlim(0,360)
+ax.set_ylim(0,256)
+#creation of an empty line for the initial plot.
+line, = ax.plot([],[])
+ani = FuncAnimation(fig, update_plot, frames=None, blit=True, interval=50)
+#receive_can_messages()
+
+can_thread = threading.Thread(target=receive_can_and_update_animation)
+can_thread.start()
 
 
-
+plt.show()
